8cbf6b5580f1b169d4a75fbc0387736f109832f9
charles-braga <chbrag27@gmail.com>
2022-05-21 15:40:56 -0300

    atualizacao .env

--- models/index.js
+++ models/index.js
@@ -1,19 +1,13 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import 'dotenv/config';
 import usuarioModel from './usuarioModel.js';
 import adocaoModel from './adocaoModel.js';
 import animalModel from './animalModel.js';
 
-/**
- * Faz a leitura do arquivo
- * ".env" por padrão
- */
-dotenv.config();
-
 const db = {};
 db.mongoose = mongoose;
 db.url = `mongodb+srv://Ucam2:12345@cluster0.aiphd.mongodb.net/Kratos?retryWrites=true&w=majority`;
-db.port = 3500;
+db.port = process.env.PORT;
 db.usuario = usuarioModel(mongoose);
 db.adocao = adocaoModel(mongoose);
 db.animal = animalModel(mongoose);
